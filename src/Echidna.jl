6392149d1fc61c1cd484634caef6fa2e35038fb0
Xijiang Yu <xijiang@users.noreply.github.com>
2020-11-25 01:43:34 +0100

    simu

---
+++ src/Echidna.jl
@@ -0,0 +1,8 @@
+module Echidna
+
+using Serialization, Statistics, Random, LinearAlgebra
+
+include("workflow.jl")
+include("simulate.jl")
+include("compare.jl")
+end # module
---
+++ src/simulate.jl
@@ -0,0 +1,57 @@
+"""
+    function simu(; nid = 100, nlc = 1000, nql = 100)
+---
+## Simulate:
+- 1000 loci
+- 100  ID
+- 100  QTL
+- Breeding values
+- VR-I G
+"""
+function simu(; nid = 100, nlc = 1000, nql = 100)
+    Z   = rand([0., 1., 2.], nid, nlc) # sample genotypes
+    qlc = sort(rand(1:nlc, nql))       # sample QTL
+    eql = randn(nql) ./ sqrt(nql)      # QTL effects
+    ID  = String[]                     # random ID names
+    for _ in 1:nid
+        push!(ID, randstring(6))
+    end
+    bv = Z[:, qlc] * eql      # breeding values
+
+    twop = mean(Z, dims=1)    # for G matrix
+    Z  .-= twop
+    s2pq = (1 .- .5twop) * twop'
+    r2pq = 1. / s2pq[1]         # mul faster than div
+    G    = Z * Z' .* r2pq + 0.0001I
+
+    ID, bv, G
+end    
+
+"""
+    function homo_R(bv; h2 = 0.8)
+---
+Simulate phenotypes with homogeneous residuals.
+"""
+function homo_R(bv; h2 = 0.8)
+    nid = length(bv)
+    vg  = var(bv)
+    ve  = vg/h2*(1-h2)
+    se  = sqrt(ve)
+    pp  = bv + randn(nid) .* se
+end
+
+"""
+    function hete_R(bv; h2 = 0.8, mo = 20)
+---
+Suppose ID may have 1:mo records
+"""
+function hete_R(bv, h2 = 0.8, mo = 20)
+    nid = length(bv)
+    nob = rand(1:mo, nid)
+    vg  = var(bv)
+    ve  = vg/h2*(1-h2)
+    se  = sqrt(ve)
+    wt  = 1 ./ sqrt.(nob)
+    pp  = bv + randn(nid) .* wt
+    nob, pp
+end
